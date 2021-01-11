d9afb95e8f970e0ad5316d026c3005133a9d3c22
lstrsrmn <59514017+lstrsrmn@users.noreply.github.com>
2021-01-11 14:55:00 +0000

    Update Mesh.cpp

--- game-engine-qt/src/graphics/Mesh.cpp
+++ game-engine-qt/src/graphics/Mesh.cpp
@@ -4,15 +4,18 @@
 #include "../../include/engine/graphics/ModelImporter.h"
 
 ModelMeshData::ModelMeshData(const std::filesystem::path &path, unsigned int id) : Asset(path, id) {
+    // recursevely loads and assigns variables to make the tree
     tree = loadModel(path);
     setMeshParent(tree);
 }
 
 ModelMeshData *ModelMeshData::createModelMeshData(const std::string &name, const std::filesystem::path &path) {
+    // requests asset manager to create modelmeshdata
     return AssetManager<ModelMeshData>::createAsset(name, path);
 }
 
 void ModelMeshData::setMeshParent(ModelMeshTree* root) {
+    // assigns this as child to parent
     for (Mesh* mesh : root->meshes) {
         mesh->parent = this;
     }
@@ -26,6 +29,7 @@ Mesh *ModelMeshData::getSubMesh(unsigned int id) {
 }
 
 Mesh *ModelMeshData::findSubMesh(unsigned int id, ModelMeshTree *root) {
+    // finds submesh
     for (Mesh* mesh : root->meshes) {
         if (mesh->id == id)
             return mesh;
@@ -40,25 +44,29 @@ Mesh *ModelMeshData::findSubMesh(unsigned int id, ModelMeshTree *root) {
 }
 
 Mesh::Mesh(Vertex *vertices, unsigned int numVertices, unsigned int *indices, unsigned int numIndices, unsigned int id, std::string name, bool sceneMode) {
+    // assigns inputs
     this->id = id;
     this->name = name;
     _drawCount = numIndices;
     QOpenGLFunctions *f = ContextController::getFunctions();
-
+    // creates necicary opengl objec ts
     VAO = new QOpenGLVertexArrayObject();
     VBO = new QOpenGLBuffer(QOpenGLBuffer::VertexBuffer);
     IBO = new QOpenGLBuffer(QOpenGLBuffer::IndexBuffer);
-
+    
+    // initialises them
     VAO->create();
     VBO->create();
     IBO->create();
-
+    
+    // code to set up opengl objects to work
     VBO->setUsagePattern(QOpenGLBuffer::StaticDraw);
     IBO->setUsagePattern(QOpenGLBuffer::StaticDraw);
 
     VAO->bind();
     VBO->bind();
     IBO->bind();
+    // allocates appropriate sizes of memory for each buffer
     IBO->allocate(indices, numIndices * sizeof(unsigned int));
     VBO->allocate(vertices, numVertices * sizeof(Vertex));
     f->glEnableVertexAttribArray(0);
@@ -73,6 +81,7 @@ Mesh::Mesh(Vertex *vertices, unsigned int numVertices, unsigned int *indices, un
 }
 
 void Mesh::draw() const {
+    // draws rasterises elements
     QOpenGLFunctions *f = ContextController::getFunctions();
     VAO->bind();
     VBO->bind();
