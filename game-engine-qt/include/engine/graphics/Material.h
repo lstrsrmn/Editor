62f320fe112b3d4e87b5b170b52ecf4bd148f43e
lstrsrmn <59514017+lstrsrmn@users.noreply.github.com>
2021-01-11 14:38:10 +0000

    Update Material.h

--- game-engine-qt/include/engine/graphics/Material.h
+++ game-engine-qt/include/engine/graphics/Material.h
@@ -7,18 +7,23 @@
 #include "../game/Transform.h"
 #include "../core/AssetManager.h"
 
+// Partially defines classes so they can be used as types
 class Texture;
 class Shader;
 
+// defines material, then defines material as an asset using the ASSET macro
 class Material : public Asset {
     ASSET(Material)
 public:
+    // defines static constructors for creating a new material and loading a prexisting material
     static Material* createMaterial(const std::string&, const std::string&, Shader*, Texture*);
     static Material* loadMaterial(const std::string&);
+    // defines the bind function
     void bind(Transform&, const Camera&, DirectionalLight* light);
     Texture* _texture;
     Shader* _shader;
 private:
+    // private constructor
     Material(const std::string&, unsigned int);
     //TODO: make multiple textures applicable
 };
