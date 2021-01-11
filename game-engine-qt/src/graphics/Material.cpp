064518882d1a12b5c3cfcd84729c2bd036e012d9
lstrsrmn <59514017+lstrsrmn@users.noreply.github.com>
2021-01-11 14:38:08 +0000

    Update Material.cpp

--- game-engine-qt/src/graphics/Material.cpp
+++ game-engine-qt/src/graphics/Material.cpp
@@ -3,11 +3,13 @@
 #include "../../include/engine/graphics/Texture.h"
 
 Material *Material::createMaterial(const std::string &name, const std::string &filePath, Shader *shader, Texture *texture) {
+    // creates and wires to properties mat file
     std::ofstream materialMat(filePath + "/" + name + ".mat");
     nlohmann::json material;
     material["texture"] = texture->getID();
     material["shader"] = shader->getID();
     materialMat << material.dump(4) << std::endl;
+    // creates a material and assigns properties
     Material* newMaterial = AssetManager<Material>::createAsset(name, filePath + "/" + name + ".mat");
     newMaterial->_shader = shader;
     newMaterial->_texture = texture;
@@ -15,21 +17,26 @@ Material *Material::createMaterial(const std::string &name, const std::string &f
 }
 
 void Material::bind(Transform &transform, const Camera &camera, DirectionalLight* light) {
+    // binds shader and textures
     _shader->bind();
     _texture->bind(0);
     _shader->update(transform, camera, light);
 }
 
 Material::Material(const std::string &filePath, unsigned int id) : Asset(filePath, id) {
+    // reads properties from file path
     std::ifstream materialMat(filePath);
     nlohmann::json material;
     materialMat >> material;
+    // loads textures and shaders
     Texture* texture = AssetManager<Texture>::getAsset(material["texture"]);
     Shader* shader = AssetManager<Shader>::getAsset(material["shader"]);
+    // assigns them locally
     _shader = shader;
     _texture = texture;
 }
 
 Material *Material::loadMaterial(const std::string &filePath) {
+    // requests material object from asset manager
     return AssetManager<Material>::getAsset(std::hash<std::string>()(filePath));
 }
