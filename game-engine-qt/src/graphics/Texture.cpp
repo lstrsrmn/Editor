e80ef92eb2e4c75afe57fab8aa913076418e8cf2
lstrsrmn <59514017+lstrsrmn@users.noreply.github.com>
2021-01-12 09:25:43 +0000

    Update Texture.cpp

--- game-engine-qt/src/graphics/Texture.cpp
+++ game-engine-qt/src/graphics/Texture.cpp
@@ -6,14 +6,16 @@
 
 Texture::Texture(const std::filesystem::path& texturePath, unsigned int id) : Asset(texturePath, id){
     QOpenGLFunctions* f = Game::instance()->getGlFunctions();
-
+    // loads in the image using opencv
     cv::Mat img = cv::imread(texturePath, cv::IMREAD_COLOR);
     cv::cvtColor(img, img, cv::COLOR_BGR2RGB);
-
+    
+    // makes sure the image loaded correctly
     if (img.data == NULL)
         std::cerr << "Texture loading failed" << std::endl;
     unsigned char* data = img.data;
 
+    // runs all nececary opengl functions to create textures
     f->glGenTextures(1, &_texture);
     f->glBindTexture(GL_TEXTURE_2D, _texture);
 
@@ -22,7 +24,8 @@ Texture::Texture(const std::filesystem::path& texturePath, unsigned int id) : As
 
     f->glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     f->glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
+    
+    // assigns the image to the opengl texture created above
     f->glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img.rows, img.cols, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 }
 
